a77a3d57755f47a08115ce2ac384d6349a8d6da0
Death Salad <ankhbhold@gmail.com>
2025-02-21 23:12:56 +0900

    First test case written.

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,19 @@
-"""Unit tests for Angets. #TODO"""
+"""Unit tests for Angets."""
+# Built-ins
+from unittest import mock
 import unittest
 
+# Angets
+import angets
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestAngets(unittest.TestCase):
+    @mock.patch('angets._core.input', create=True)
+    def test_get_non_empty_str(self, mocked_input):
+        # Raise exception when all 3 inputs are empty.
+        with self.assertRaises(angets.exceptions.AttemptsExceededError):
+            mocked_input.side_effect = ['', '', '']
+            angets.get_non_empty_str(attempts=3)
 
 
 if __name__ == '__main__':
